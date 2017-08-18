34a336430dbd7c04144d1f5c0ca8b7934d086519
MaBeier <JemmaJemma@yeah.net>
2017-08-18 13:27:51 +0800

    js部分更新

--- js/js.js
+++ js/js.js
@@ -1,5 +1,3 @@
-
-
 var screenAnimateElements={
   '.sr-1':[
       '.sr_container-1' ,
@@ -35,22 +33,44 @@ var screenAnimateElements={
 };
 
 
+var navItems=document.querySelectorAll('.nav_item');
+var outLineItems=document.querySelectorAll('.outline_item');
+var navTip=document.querySelector('.header_nav-tip');
+var outline=document.querySelector('.outline');
 
 
+var unit=(function(){
+    var delCls=function(e,cls){
+          var baseCls=e.getAttribute('class');
+          if(cls.indexOf(cls)>-1)
+         return e.setAttribute('class',baseCls.split(cls).join(' ').replace(/\s+/g,' ') )
+        }
 
 
+    var addCls=function(e,cls){
+        var baseCls=e.getAttribute('class');
+        if(baseCls.indexOf(cls)===-1)
+        return e.setAttribute('class',baseCls+' '+cls);
+      }
 
-setTimeout(function(){playScreenAnimateDone('.sr-1');},100)
+      return {
+        delCls:delCls,
+        addCls:addCls
+      }
+})()
+
 
 
 
 //初始化
 window.onload=function(){
   //设置成init
-  for(k in screenAnimateElements){
+  for(k in screenAnimateElements){//console.log(k);
     setScreenAnimateInit(k);
   }
 }
+
+
 
 function setScreenAnimateInit(screenCls){
   var screen=document.querySelector(screenCls);
@@ -63,6 +83,7 @@ function setScreenAnimateInit(screenCls){
 }
 
 
+setTimeout(function(){playScreenAnimateDone('.sr-1')},100)
 
 
 //滚动条滑动
@@ -76,34 +97,37 @@ function playScreenAnimateDone(screenCls){
   }
 }
 
-
+ 
 
 //滚动条动
 window.onscroll = function(){
   var top  = document.body.scrollTop;
   switchNavItemsActive(0);
-  if(top<640-80){
+  if(top<620){
     playScreenAnimateDone('.sr-1');
     switchNavItemsActive(0);
+    unit.delCls(outline,'outline_status_in')
   }
  
-  if(top>640-80){
+  if(top>620){
     playScreenAnimateDone('.sr-2');
     switchNavItemsActive(1);
+    unit.addCls(outline,'outline_status_in')
+
   }
-  if(top>(640*2-80)){
+  if(top>(1240)){
     playScreenAnimateDone('.sr-3');
     switchNavItemsActive(2);
   }
-  if(top>(640*3-80)){
+  if(top>(1860)){
     playScreenAnimateDone('.sr-4');
     switchNavItemsActive(3);
   }
-  if(top>(640*4-80)){
+  if(top>(2480)){
     playScreenAnimateDone('.sr-5');
     switchNavItemsActive(4);
   }
-    if(top>(640*4+20)){
+    if(top>(2800)){
     switchNavItemsActive(5);
   }
 }
@@ -111,38 +135,18 @@ window.onscroll = function(){
 
 
 
-var navItems=document.querySelectorAll('.nav_item');
-var outLineItems=document.querySelectorAll('.outline_item');
 
-
-
-
-function delCls(e,cls){
-  var baseCls=e.getAttribute('class');
-  if(cls.indexOf(cls)>-1){
-    e.setAttribute('class',baseCls.split(cls).join(' ').replace(/\s+/g,' ') );
-  }
-}
-
-
-function addCls(e,cls){
-  var baseCls=e.getAttribute('class');
-  if(baseCls.indexOf(cls)===-1)
-  e.setAttribute('class',baseCls+' '+cls);
-}
 
 
 
 function switchNavItemsActive(idx){
   for(var i=0;i<navItems.length;i++){
-    delCls(navItems[i],'header_nav-item_active');
-    delCls(outLineItems[i],'outline_item_status_active');
-    //console.log('i',i);
+    unit.delCls(navItems[i],'header_nav-item_active');
+    unit.delCls(outLineItems[i],'outline_item_status_active');
   }
-  addCls(navItems[idx],'header_nav-item_active');
-  addCls(outLineItems[idx],'outline_item_status_active');
-  navTip.style.left=( idx * 115 )+'px';
-  //console.log('idx',idx);
+  unit.addCls(navItems[idx],'header_nav-item_active');
+  unit.addCls(outLineItems[idx],'outline_item_status_active');
+  navTip.style.left=( idx * 113.5 )+'px';
 }
 
 
@@ -154,38 +158,28 @@ function setNavJump(i,lib){
       document.body.scrollTop=0; 
     }
     else
-      {document.body.scrollTop=(i+1)*640-80;
+      {document.body.scrollTop=i*640;
     switchNavItemsActive(i);}
   }
 }
 
 
-
-
-for(var i=0;i<navItems.length;i++){
-  setNavJump(i,navItems);
-}
-
-//大纲点击跳转
-for(var i=0;i<outLineItems.length;i++){
-  setNavJump(i,outLineItems);
-}
-
-
-
 //滑动门
-var navTip=document.querySelector('.header_nav-tip'); 
-
-function headerNavTip(){
+function headerNavTip(i){
   var j=i;
   navItems[j].onmouseover=function(){
-    navTip.style.left=(i*115)+'px';
+    navTip.style.left=(i*113.5)+'px';
     switchNavItemsActive(j);
   }
 }
 
+
 for(var i=0;i<navItems.length;i++){
-  headerNavTip(i);
+    setNavJump(i,navItems);//console.log(i,navItems)
+    setNavJump(i,outLineItems);
+    headerNavTip(i);
 }
+
+
 
 
